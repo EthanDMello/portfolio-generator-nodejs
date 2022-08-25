6a1853d34b8253b5add9cb3a04be2de8674e1168
TheBiggestThereIs <107923914+EthanDMello@users.noreply.github.com>
2022-08-25 21:46:17 +0100

    write file

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { error } = require("console");
+
+const generateHTML = (username, location, age) => {
+  `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Generated Portfolio</title>
+  </head>
+  <body>
+    <h1>${username}</h1>
+    <p>I live in ${location}</p>
+    <p>I am ${age}</p>
+  </body>
+</html>
+`;
+};
+
+inquirer
+  .prompt([
+    { name: "username", message: "What is your name?", type: "input" },
+    { name: "location", message: "Where do you live?", type: "input" },
+    { name: "age", message: "How old are you?", type: "input" },
+  ])
+  .then((response) => {
+    const text = generateHTML(response);
+    fs.writeFile("index.html", text, (err) => {
+      err ? console.log(err) : console.log("Success");
+    });
+  });
